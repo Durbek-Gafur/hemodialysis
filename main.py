301fedd9bfc5a4ee6e8fed3a9698c1782cb819fb
Durbek-Gafur <durbek.gafurov@gmail.com>
2021-11-07 00:34:20 -0500

    Added awesome bar chart

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from text import write
 from bloodAndSolution import *
 
 pygame.init()
-DIM = np.asarray([800, 400])
+DIM = np.asarray([800, 600])
 screen = pygame.display.set_mode(DIM)
 pygame.display.set_caption('E-kidney')
 running = True
 startTime = datetime.datetime.now()
 bloodClean = False
+
 # keeps running until quitting
 while running:
 	for event in pygame.event.get():
@@ -25,15 +26,23 @@ while running:
 		# write(screen,"NORMALISED",(screen.get_width()//4, screen.get_height()//2+45))
 		pygame.display.flip()
 	else:
-		screen.fill((255,204,204), (0, 0, screen.get_width()// 2, screen.get_height()))#-200
-		screen.fill((204,204,204), (DIM[1]+1, 0, screen.get_width()// 2, screen.get_height()))
-		write(screen,"BLOOD",(screen.get_width()//4, screen.get_height()//2))
-		write(screen,"DIALYSIS SOLUTION",(screen.get_width()//4+screen.get_width()//2, screen.get_height()//2))
+		screen.fill((255,255,255), (0, 0, 800,600))
+		screen.fill((255,204,204), (0, 0, screen.get_width()// 2, 400))#-200
+		screen.fill((204,204,204), (401, 0, screen.get_width()// 2, 400))
+		write(screen,"BLOOD",(screen.get_width()//4, 200))
+		write(screen,"DIALYSIS SOLUTION",(screen.get_width()//4+screen.get_width()//2, 200))
 		ms = str(datetime.datetime.now()-startTime)[:10]
 		write(screen,ms,(screen.get_width()//2, 25))
 		blood.update()
 		blood.display(screen)
 		solution.update()
 		solution.display(screen)
+		
+		level = 430
+		start = 400
+		for electrolyte in NORMS:
+			pygame.draw.line(screen, NORMS[electrolyte][1], (start, level), (start+(50*(blood.countParticle(electrolyte) - NORMS[electrolyte][0])), level),20)
+			level+=30
+
 		pygame.display.flip()
 
